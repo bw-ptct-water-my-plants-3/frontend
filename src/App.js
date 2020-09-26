4c6ec64658331614906314c95fcfd9ccf655e596
barinder singh <tsothalanti2@yahoo.com>
2020-09-25 19:21:42 -0700

    imported RegisterUser component into App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import './App.css';
-import Home from "./components/Home"
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from './components/Login';
+import Login from "./components/Login";
+import RegisterUser from "./components/RegisterUser";
 
 function App() {
   return (
     <div className="App">
-        <Router>
-          
-          
-          <Switch>
-            
-            <Route path="/" exact component={Home} />
-            <Route path="/login" component={Login}/>
-             
-          </Switch>
-  
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={RegisterUser}/>
+        </Switch>
       </Router>
-      
     </div>
   );
 }
