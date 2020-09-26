470449302eead056c1bc039e04211f449cc3bff7
barinder singh <tsothalanti2@yahoo.com>
2020-09-25 19:19:16 -0700

    created basic form for registering a user

--- src/components/RegisterUser.js
+++ src/components/RegisterUser.js
@@ -1,11 +1,53 @@
 import React from "react";
+import { Button } from "reactstrap";
 
 const RegisterUser = () => {
   const [register, setRegister] = useState({
-      username: "",
-      password: "",
-      phoneNumber: ""
+    username: "",
+    password: "",
+    phoneNumber: "",
   });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log();
+    setRegister({ ...register, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    console.log(register);
+    e.preventDefault();
+    alert("Thank you for registering!");
+  };
+
+  return (
+    <div>
+      <h1>Register for an account!</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          onchange={handleChange}
+        />
+        <input
+          type="text"
+          value={register.password}
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={register.text}
+          placeholder="phone number"
+          name="phoneNumber"
+          onChange={handleChange}
+        />
+        <Button color="success">REGISTER</Button>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterUser;
