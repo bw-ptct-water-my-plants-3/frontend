0b7def289eabf791fcd407dcfdf7bc1c51f6e582
William Sedlacek <wsedlacekc@gmail.com>
2020-09-27 14:10:30 -0700

    fix: hookup add plant link
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/components/PlantDashboard.js
+++ src/components/PlantDashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 
 import PlantList from './PlantList';
@@ -7,7 +8,9 @@ export default () => {
   return (
     <div className="margin">
       <div className="flex-end gap-y">
-        <Button color="primary">Add</Button>
+        <Link to="/plants/new">
+          <Button color="primary">Add</Button>
+        </Link>
       </div>
       <PlantList className="gap-y"></PlantList>
     </div>
